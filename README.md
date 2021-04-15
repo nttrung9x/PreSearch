3c1076b3fb462584dda57c246569403e2b5b202f
SK Zic <64146697+skzic@users.noreply.github.com>
2021-04-16 01:02:11 +0700

    AAE first commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # PreSearch
-PreSearch
+
+### [EN - Tiếng Việt bên dưới]
+
+##### PreSearch Automation Search (Give away full source)
+
+Start to claim **PRE** token https://coinmarketcap.com/currencies/presearch/ from now!
+
+Daily, you have random 30 times which is rewarded 0.12 **PRE**. So after 1 month, you may receive 108 PRE ~ 10 dollars (1 **PRE** = 0.09882 dollar). But, PreSearch is not good enough from Google or Bing so it's not comfortable to use!
+
+###### Functions:
+1. Search 30 times per hour.
+2. Auto-click to random search results then come back.
+
+
+###### Please do step by step to use:
+1. Install language **Python 3.9** https://www.python.org/downloads/. For more help, please refer.
+2. Install modules **selenium**, **win10toast** `python -m pip install selenium win10toast`. For more help, please refer.
+3. (_Optional_) Install IDE **PyCharm** https://www.jetbrains.com/pycharm/. For more help, please refer.
+4. (_Optional_) Download **Chromium Webdriver** `https://chromedriver.chromium.org/`. For more help, please refer.
+5. Run script.
+6. (_Optional_) Edit script for your personal use. Contact me for more functions.
+
+### [VN - English above]
+
+##### Tìm kiếm tự động với PreSearch (Tặng đầy đủ mã nguồn)
+
+Bắt đầu nhận **PRE** token https://coinmarketcap.com/currencies/presearch/ ngay bây giờ!
+
+Hằng ngày, bạn có 30 lượt tìm kiếm ngẫu nhiên được tặng 0.12 **PRE**. Nên sau 1 tháng, bạn có thể nhận được 108 PRE ~ 10 dollars (1 **PRE** = 0.09882 đô). Song, PreSearch không đủ tốt so với Google hay Bing nên không thoải mái lắm khi sử dụng!
+
+###### Tính năng:
+1. Tìm 30 lần mỗi giờ.
+2. Tự động click ngẫu nhiên vào kết quả tìm kiếm và quay trở lại.
+
+###### Vui lòng thực hiện theo các bước sau:
+1. Cài đặt ngôn ngữ **Python 3.9** https://www.python.org/downloads/. Xem thêm trợ giúp tại.
+2. Cài đặt modules **selenium**, **win10toast** `python -m pip install selenium win10toast`. Xem thêm trợ giúp tại.
+3. (_Tự chọn_) Cài đặt IDE **PyCharm** https://www.jetbrains.com/pycharm/. Xem thêm trợ giúp tại.
+4. (_Tự chọn_) Tải **Chromium Webdriver** `https://chromedriver.chromium.org/`. Xem thêm trợ giúp tại.
+5. Thực thi mã.
+6. (_Tự chọn_) Sửa mã cho mục đích sử dụng cá nhân của bạn. Liên hệ tôi để có thêm tính năng mới!
+
+# Auto Almost Everything
+
+Email: autoalmosteverything.2021@gmail.com
+
+
+Github: https://github.com/srcaae?tab=repositories
+
+
+Youtube: https://www.youtube.com/channel/UC4cNnZIrjAC8Q4mcnhKqPEQ
+
+
+Facebook: https://www.facebook.com/ghienaae
---
+++ main.py
@@ -0,0 +1,126 @@
+# Auto Almost Everything
+# Youtube Channel https://www.youtube.com/channel/UC4cNnZIrjAC8Q4mcnhKqPEQ
+# Facebook Community https://www.facebook.com/ghienAAE
+# Github Source Code https://github.com/srcaae?tab=repositories
+# Please read README.md carefully before use
+
+import random
+import threading
+import time
+
+from selenium.webdriver.common.keys import Keys  # python -m pip install selenium
+from win10toast import ToastNotifier  # python -m pip install win10toast
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chromedriver_path = '.\\chromedriver.exe'
+opts = Options()
+opts.binary_location = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'  # <-- Change to your Chromium browser path, replace "\" with "\\".
+opts.add_experimental_option('excludeSwitches', ['enable-automation'])
+opts.add_experimental_option('useAutomationExtension', False)
+
+
+# Notification
+def Notification(app, content):
+    try:
+        toast = ToastNotifier()
+        toast.show_toast(app, content, duration=6)
+    except:
+        pass
+
+
+sync = True
+
+
+# Search 30 times per hour
+def PreSearch():
+    app = 'PreSearch'
+    path = 'https://engine.presearch.org'
+    presearch_cookies = [
+        {
+            'name': 'remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d',
+            # Replace by your remember token -->
+            'value': 'YourRememberTokenHere',
+            # <-- Replace by your remember token
+            'domain': '.presearch.org',
+            'path': '/',
+        },
+        {
+            'name': 'token',
+            # Replace by your token -->
+            'value': 'YourTokenHere',
+            # <-- Replace by your token
+            'domain': '.presearch.org',
+            'path': '/',
+        },
+    ]
+    word_site = 'https://www.mit.edu/~ecprice/wordlist.10000'
+    response = requests.get(word_site)
+    word_list = response.content.splitlines()
+    presearch_max_count = 30
+
+    while True:
+        global sync
+        if sync:
+            sync = False
+            browser = webdriver.Chrome(options=opts, executable_path=chromedriver_path)
+            browser.set_page_load_timeout(60)
+            try:
+                browser.get(path)
+                time.sleep(1)
+                for cookie in presearch_cookies:
+                    browser.add_cookie(cookie)
+                browser.get(path)
+                time.sleep(1)
+                count = 0
+                while True:
+                    browser.get(path)
+                    time.sleep(3)
+                    q = ' '.join(
+                        [word_list[random.randint(0, len(word_list))].decode() for j in range(random.randint(1, 6))])
+                    try:
+                        browser.find_element_by_xpath("//input[@placeholder='Search']").send_keys(q, Keys.ENTER)
+                        time.sleep(3)
+                        if 'Oops, something went wrong with your search' not in browser.page_source and 'The request could not be satisfied' not in browser.title:
+                            for i in range(random.randint(1, 3)):
+                                try:
+                                    results = browser.find_elements_by_xpath(
+                                        "//h3[@class='font-semibold text-lg text-primary-600 transition duration-150 hover:underline hover:opacity-70 dark:hover:opacity-60 dark:text-blue-200 dark:font-normal']")
+                                    results[random.randint(0, len(results) - 1)].click()
+                                    time.sleep(3)
+                                    browser.back()
+                                    time.sleep(3)
+                                except:
+                                    pass
+                            count += 1
+                            time.sleep(3)
+                    except:
+                        pass
+                    if count > presearch_max_count * 120 / 100:
+                        Notification(app, 'Searched 30 times!')
+                        break
+            except Exception as ex:
+                print('%s has exception:\n%s!' % (app, ex))
+                Notification(app, '%s has exception:\n%s!' % (app, ex))
+            finally:
+                browser.quit()
+            sync = True
+            time.sleep(7200)
+        else:
+            time.sleep(1)
+
+
+try:
+    threads = []
+    threads.append(threading.Thread(target=PreSearch, args=()))
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+except Exception as ex:
+    print('Threading has exception:\n%s!' % ex)
+
+# Please Like Facebook, Subscribe & Like Youtube channel
+# Contact me: autoalmosteverything.2021@gmail.com
